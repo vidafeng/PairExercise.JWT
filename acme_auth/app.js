f1b02c3e0ea448d4c993e8a7c0eead07da58dd24
Vida Feng <vidaffeng@gmail.com>
2022-04-04 17:58:13 -0400

    Making the Call from React

--- acme_auth/app.js
+++ acme_auth/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(express.json());
 const {
-  models: { User },
+  models: { User, Note },
 } = require("./db");
 const path = require("path");
+const { notEqual } = require("assert");
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
@@ -17,17 +19,23 @@ app.post("/api/auth", async (req, res, next) => {
   }
 });
 
-app.get("/api/auth", async (req, res, next) => {
+app.get("/api/:userid/notes", async (req, res, next) => {
   try {
-    res.send(await User.byToken(req.headers.authorization));
-  } catch (ex) {
-    next(ex);
+    res.send(
+      await Note.findAll({
+        where: {
+          userId: req.params.userid,
+        },
+      })
+    );
+  } catch (err) {
+    next(err);
   }
 });
 
-app.delete("/api/auth", async (res, res, next) => {
+app.get("/api/auth", async (req, res, next) => {
   try {
-    res.send();
+    res.send(await User.byToken(req.headers.authorization));
   } catch (ex) {
     next(ex);
   }
--- acme_auth/db.js
+++ acme_auth/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const { STRING } = Sequelize;
 const config = {
@@ -19,10 +20,19 @@ const User = conn.define("user", {
   password: STRING,
 });
 
+const Note = conn.define("note", {
+  text: STRING,
+});
+
+// Associations
+User.hasMany(Note);
+Note.belongsTo(User);
+
 User.byToken = async (token) => {
   try {
     // returns data (actual user)
-    const verify = jwt.verify(token, process.env.jwt);
+    console.log("token", token);
+    const verify = await jwt.verify(token, process.env.JWT);
 
     const user = await User.findByPk(verify.userId);
 
@@ -40,17 +50,18 @@ User.byToken = async (token) => {
 };
 
 User.authenticate = async ({ username, password }) => {
-  // this is supposed to return jwt
-
   try {
     const user = await User.findOne({
       where: {
         username,
-        password,
       },
     });
-    const token = jwt.sign({ userId: user.id }, process.env.jwt);
-    return token;
+    const result = await bcrypt.compare(password, user.password);
+    // if hashed pw is the same as plain text pw
+    if (result) {
+      // return the token and allow sign in
+      return await jwt.sign({ userId: user.id }, process.env.JWT);
+    }
   } catch (error) {
     error = Error("bad credentials");
     error.status = 401;
@@ -58,8 +69,25 @@ User.authenticate = async ({ username, password }) => {
   }
 };
 
+User.beforeCreate(async (user) => {
+  const saltRounds = 10;
+  // third argument not necessary, why?
+  const hash = await bcrypt.hash(user.password, saltRounds);
+  user.password = hash;
+
+  //   console.log(user.password);
+
+  //   await bcrypt.genSalt(saltRounds, function (err, salt) {
+  //     bcrypt.hash(user.password, salt, function (err, hash) {
+  //       user.password = hash;
+  //     });
+  //     console.log(user.password);
+  //   });
+});
+
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
+
   const credentials = [
     { username: "lucy", password: "lucy_pw" },
     { username: "moe", password: "moe_pw" },
@@ -68,6 +96,20 @@ const syncAndSeed = async () => {
   const [lucy, moe, larry] = await Promise.all(
     credentials.map((credential) => User.create(credential))
   );
+
+  const notes = [
+    { text: "I should walk my dog" },
+    { text: "This pair exercise is quite hard" },
+    { text: "Cookies sound great right now" },
+  ];
+
+  const [note1, note2, note3] = await Promise.all(
+    notes.map((note) => Note.create(note))
+  );
+
+  await lucy.setNotes(note1);
+  await moe.setNotes([note2, note3]);
+
   return {
     users: {
       lucy,
@@ -81,5 +123,6 @@ module.exports = {
   syncAndSeed,
   models: {
     User,
+    Note,
   },
 };
